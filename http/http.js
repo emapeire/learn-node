fa9634c7e08f9b23107c83f5f0aabb53e0e8ae91
Emanuel Peire <emapeire@outlook.com>
2023-12-06 17:24:00 -0300

    Update HTTP server to use desired port

--- http/http.js
+++ http/http.js
@@ -1,12 +1,14 @@
 const http = require('node:http')
 const { findAvailablePort } = require('../free-port/free-port.js')
 
+const desiredPort = process.env.PORT ?? 3000
+
 const server = http.createServer((req, res) => {
   console.log('Request made')
   res.end('Hello World')
 })
 
-findAvailablePort(3000).then((port) => {
+findAvailablePort(desiredPort).then((port) => {
   server.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`)
   })
